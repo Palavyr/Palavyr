bcee6d679b7677f12620c757fc1627decb24c381
Paul Gradie <paul.e.gradie@gmail.com>
2020-09-23 12:49:52 +1000

    revert[0]

--- server/generateMigrationScripts.ps1
+++ server/generateMigrationScripts.ps1
@@ -5,6 +5,6 @@ catch {
     Write-Host "Tool already installed" -ForegroundColor RED
 }
 
-dotnet-ef migrations script --project Palavyr.API --context AccountsContext --output accounts_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
-dotnet-ef migrations script --project Palavyr.API --context DashContext --output configuration_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
-dotnet-ef migrations script --project Palavyr.API --context ConvoContext --output convo_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
+dotnet ef migrations script --project ./Palavyr.API --context AccountsContext --output accounts_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
+dotnet ef migrations script --project ./Palavyr.API --context DashContext --output configuration_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
+dotnet ef migrations script --project ./Palavyr.API --context ConvoContext --output convo_migration.%dep.ConfigurationManager_CreateVersionNumber.system.build.number%.sql --idempotent
