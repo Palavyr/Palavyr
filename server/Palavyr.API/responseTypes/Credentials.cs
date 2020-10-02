b1353cce7b79dbe5eabda3c957c375837d41218c
Paul Gradie <paul.e.gradie@gmail.com>
2020-10-02 21:55:39 +1000

    directory name case change[0]

---
+++ server/Palavyr.API/responseTypes/Credentials.cs
@@ -0,0 +1,29 @@
+﻿namespace Palavyr.API.ResponseTypes
+{
+    public class Credentials
+    {
+        public string ApiKey { get; set; }
+        public string SessionId { get; set; } // used to access the correct database
+        public bool Authenticated { get; set; }
+        public string Message { get; set; }
+
+
+        Credentials(string sessionId, string apiKey, bool authenticated, string message)
+        {
+            ApiKey = apiKey;
+            SessionId = sessionId;
+            Authenticated = authenticated;
+            Message = message;
+        }
+
+        public static Credentials CreateAuthenticatedResponse(string sessionId, string apiKey)
+        {
+            return new Credentials(sessionId, apiKey, true, "Authentication Successful");
+        }
+
+        public static Credentials CreateUnauthenticatedResponse(string message)
+        {
+            return new Credentials("", "", false, message);
+        }
+    }
+}
---
+++ server/Palavyr.API/responseTypes/FileLink.cs
@@ -0,0 +1,21 @@
+﻿namespace Palavyr.API.ResponseTypes
+{
+    public class FileLink
+    {
+        public string Link { get; set; }
+        public object FileName { get; set; }
+        public string FileId { get; set; }
+        
+        private FileLink(string fileName, string link, string fileId)
+        {
+            FileName = fileName;
+            Link = link;
+            FileId = fileId;
+        }
+        
+        public static FileLink CreateLink(string fileName, string link, string fileId)
+        {
+            return new FileLink(fileName, link, fileId);
+        }
+    }
+}
