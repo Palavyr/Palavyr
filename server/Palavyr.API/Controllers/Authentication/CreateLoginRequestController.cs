3a7ae1181934e737c5db0dc07873b0523fbaaf0a
Paul Gradie <paul.e.gradie@gmail.com>
2021-01-01 19:03:58 +1100

    add minor logging to login controller

--- server/Palavyr.API/Controllers/Authentication/CreateLoginRequestController.cs
+++ server/Palavyr.API/Controllers/Authentication/CreateLoginRequestController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Palavyr.API.RequestTypes;
 using Palavyr.API.ResponseTypes;
 using Palavyr.API.Services.AuthenticationServices;
@@ -11,20 +12,24 @@ namespace Palavyr.API.Controllers.Authentication
     [ApiController]
     public class CreateLoginRequestController : ControllerBase
     {
+        private readonly ILogger<CreateLoginRequestController> logger;
         private readonly IAuthService authService;
 
         public CreateLoginRequestController(
+            ILogger<CreateLoginRequestController> logger,
             IAuthService authService
         )
         {
+            this.logger = logger;
             this.authService = authService;
         }
+
         [AllowAnonymous]
         [HttpPost("authentication/login")]
         public async Task<Credentials> RequestLogin([FromBody] LoginCredentials credentials)
         {
+            logger.LogDebug("Login Request Received.");
             return await authService.PerformLoginAction(credentials);
         }
-        
     }
 }
