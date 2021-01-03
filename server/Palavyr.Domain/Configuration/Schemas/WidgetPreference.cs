d8d49abfa83fe36e28ca9642ac806af9ab5cb16c
Paul Gradie <paul.e.gradie@gmail.com>
2021-01-03 11:40:23 +1100

    add migrations for widget pref cols

--- server/Palavyr.Domain/Configuration/Schemas/WidgetPreference.cs
+++ server/Palavyr.Domain/Configuration/Schemas/WidgetPreference.cs
@@ -26,20 +26,32 @@ namespace Server.Domain.Configuration.Schemas
         [DefaultValue("#E1E1E1")]
         public string SelectListColor { get; set; }
         
+        [DefaultValue("red")]
+        public string ListFontColor { get; set; }
+        
         [DefaultValue("#35CCE6")]
         public string HeaderColor { get; set; }
-        
-        [DefaultValue("Architects Daughter")]
-        public string FontFamily { get; set; }
         
         [DefaultValue("black")]
         public string HeaderFontColor { get; set; }
         
-        [DefaultValue("red")]
-        public string ListFontColor { get; set; }
+        [DefaultValue("Architects Daughter")]
+        public string FontFamily { get; set; }
+        
+        [DefaultValue("#35CCE6")]
+        public string OptionsHeaderColor { get; set; }
+        
+        [DefaultValue("black")]
+        public string OptionsHeaderFontColor { get; set; }
+        
+        [DefaultValue("#35CCE6")]
+        public string ChatFontColor { get; set; }
+        
+        [DefaultValue("#35CCE6")]
+        public string ChatBubbleColor { get; set; }
         
         public bool WidgetState { get; set; }
-
+        
         WidgetPreference()
         {
         }
