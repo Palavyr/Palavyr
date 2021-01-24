91898f57598a88aa2dd0eca8a0c3e54da564e00c
Paul Gradie <paul.e.gradie@gmail.com>
2021-01-25 00:13:47 +1100

    minor fixes

--- server/Palavyr.Data.Migrator/DataMigrator.cs
+++ server/Palavyr.Data.Migrator/DataMigrator.cs
@@ -19,53 +19,57 @@ namespace Palavyr.Data.Migrator
         private const string ConfigFilterName = "configuration_migration";
 
         private const string Environment = "Environment";
-        
-        private static ILogger _logger { get; set; }
+
+        private static ILogger<DataMigrator> Logger { get; set; }
 
         static int Main(string[] args)
-        {
-            using var loggerFactory = LoggerFactory.Create(builder =>
-            {
-                builder
-                    .AddFilter("Microsoft", LogLevel.Warning)
-                    .AddFilter("System", LogLevel.Warning)
-                    .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
-                    .AddConsole()
-                    .AddEventLog();
-            });
-            _logger = loggerFactory.CreateLogger<DataMigrator>();
+        { 
+            var loggerFactory = LoggerFactory.Create(
+                builder =>
+                {
+                    builder
+                        .AddFilter("Microsoft", LogLevel.Warning)
+                        .AddFilter("System", LogLevel.Warning)
+                        .AddFilter("Palavyr.Data.Migrator.DataMigrator", LogLevel.Debug)
+                        .AddConsole()
+                        .AddEventLog();
+                });
+            Logger = loggerFactory.CreateLogger<DataMigrator>();
+            Logger.LogDebug("This is the first thing that happens. A TEST.");
             var assembly = Assembly.GetExecutingAssembly();
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.migrator.json", true)
                 .AddUserSecrets(assembly, true)
                 .Build();
-            _logger.LogInformation("This is a test of the logging system.");
-            var env = configuration.GetValue<string>("Environment");
-            _logger.LogDebug($"This is a debug test to print the env... printing: {env}");
-            _logger.LogInformation($"Data Migrations being performed in {env}");
+
+            Logger.LogInformation("This is a test of the logging system.");
+            var env = configuration.GetValue<string>(Environment);
+
+            Logger.LogDebug($"This is a debug test to print the env... printing: {env}");
+            Logger.LogInformation($"Data Migrations being performed in {env}");
 
             var accountsRes = ApplyMigrations(env, AccountConfigKey, AccountFilterName, configuration);
             if (accountsRes == -1) return -1;
-            
+
             var convoRes = ApplyMigrations(env, ConvoConfigKey, ConvoFilterName, configuration);
             if (convoRes == -1) return -1;
-            
+
             var configRes = ApplyMigrations(env, ConfigConfigKey, ConfigFilterName, configuration);
             if (configRes == -1) return -1;
-            _logger.LogInformation("Successfully updated the database!");
+            Logger.LogInformation("Successfully updated the database!");
             return 0;
         }
 
         private static int ApplyMigrations(string env, string configKey, string filterName, IConfiguration config)
         {
-            _logger.LogInformation($"Deploying migration for {configKey} in {env}.");
+            Logger.LogInformation($"Deploying migration for {configKey} in {env}.");
             var connection = config.GetConnectionString(configKey);
-            _logger.LogInformation($"Connection String: {connection}");
+            Logger.LogInformation($"Connection String: {connection}");
             EnsureDatabase.For.PostgresqlDatabase(connection);
             var accountsRes = DeployMigration(connection, filterName);
             return accountsRes;
         }
-        
+
         private static int DeployMigration(string connectionString, string filterName)
         {
             var upgrader =
@@ -79,13 +83,12 @@ namespace Palavyr.Data.Migrator
             var result = upgrader.PerformUpgrade();
 
             if (result.Successful) return 0;
-            _logger.LogCritical($"Encountered error while running migration for {connectionString}. Error: {result.Error}");
+            Logger.LogCritical($"Encountered error while running migration for {connectionString}. Error: {result.Error}");
             Console.ResetColor();
 #if DEBUG
             Console.ReadLine();
-#endif                
+#endif
             return -1;
-
         }
     }
 }
