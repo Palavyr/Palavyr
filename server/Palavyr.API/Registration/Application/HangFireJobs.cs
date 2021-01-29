fab85a99eca65a2e2844f7a4702f786d16dd8951
Paul Gradie <paul.e.gradie@gmail.com>
2021-01-29 22:32:16 +1100

    wip

--- server/Palavyr.API/Registration/Application/HangFireJobs.cs
+++ server/Palavyr.API/Registration/Application/HangFireJobs.cs
@@ -31,43 +31,44 @@ namespace Palavyr.API.Registration.Application
 
         public void AddHangFireJobs(IApplicationBuilder app)
         {
-            if (env.IsProduction())
-            {
-                logger.LogDebug("Current think its production Okay?");
-                logger.LogDebug("Setting up the hangfire dashboard");
-                app.UseHangfireDashboard();
+            // if (env.IsProduction())
+            // {
+            // logger.LogDebug("Current think its production Okay?");
+            logger.LogDebug($"Current env for hangfire: {env}");
+            logger.LogDebug("Setting up the hangfire dashboard");
+            app.UseHangfireDashboard();
 
-                recurringJobManager
-                    .AddOrUpdate(
-                        "Backup database",
-                        () => serviceProvider.GetService<ICreatePalavyrSnapshot>().CreateAndTransferCompleteBackup(),
-                        Cron.Minutely
-                    );
-                recurringJobManager
-                    .AddOrUpdate(
-                        "Keep only the last 50 snapshots",
-                        () => serviceProvider.GetService<IRemoveOldS3Archives>().RemoveS3Objects(),
-                        Cron.Daily
-                    );
-                recurringJobManager
-                    .AddOrUpdate(
-                        "Clean Expired Sessions",
-                        () => serviceProvider.GetService<IRemoveStaleSessions>().CleanSessionDB(),
-                        Cron.Hourly
-                    );
-                recurringJobManager
-                    .AddOrUpdate(
-                        "Validate All Attachment DB Entries",
-                        () => serviceProvider.GetService<IValidateAttachments>().ValidateAllAttachments(),
-                        Cron.Weekly
-                    );
-                recurringJobManager
-                    .AddOrUpdate(
-                        "Validate All Files",
-                        () => serviceProvider.GetService<IValidateAttachments>().ValidateAllFiles(),
-                        Cron.Weekly
-                    );
-            }
+            recurringJobManager
+                .AddOrUpdate(
+                    "Backup database",
+                    () => serviceProvider.GetService<ICreatePalavyrSnapshot>().CreateAndTransferCompleteBackup(),
+                    Cron.Minutely
+                );
+            recurringJobManager
+                .AddOrUpdate(
+                    "Keep only the last 50 snapshots",
+                    () => serviceProvider.GetService<IRemoveOldS3Archives>().RemoveS3Objects(),
+                    Cron.Daily
+                );
+            recurringJobManager
+                .AddOrUpdate(
+                    "Clean Expired Sessions",
+                    () => serviceProvider.GetService<IRemoveStaleSessions>().CleanSessionDB(),
+                    Cron.Hourly
+                );
+            recurringJobManager
+                .AddOrUpdate(
+                    "Validate All Attachment DB Entries",
+                    () => serviceProvider.GetService<IValidateAttachments>().ValidateAllAttachments(),
+                    Cron.Weekly
+                );
+            recurringJobManager
+                .AddOrUpdate(
+                    "Validate All Files",
+                    () => serviceProvider.GetService<IValidateAttachments>().ValidateAllFiles(),
+                    Cron.Weekly
+                );
+            // }
         }
     }
 }
