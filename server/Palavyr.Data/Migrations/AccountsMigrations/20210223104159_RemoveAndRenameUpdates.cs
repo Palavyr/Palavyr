7f3d4a122f0448b09634e2a5190b6706e320215d
Paul Gradie <paul.e.gradie@gmail.com>
2021-02-23 21:43:37 +1100

    rename and remove unused db cols

---
+++ server/Palavyr.Data/Migrations/AccountsMigrations/20210223104159_RemoveAndRenameUpdates.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Palavyr.Data.Migrations.AccountsMigrations
+{
+    public partial class RemoveAndRenameUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Accounts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Accounts",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
---
+++ server/Palavyr.Data/Migrations/ConfigurationMigrations/20210223104207_RemoveAndRenameUpdates.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Palavyr.Data.Migrations.ConfigurationMigrations
+{
+    public partial class RemoveAndRenameUpdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccountId = table.Column<string>(type: "text", nullable: true),
+                    GroupId = table.Column<string>(type: "text", nullable: true),
+                    GroupName = table.Column<string>(type: "text", nullable: true),
+                    ParentId = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+        }
+    }
+}
