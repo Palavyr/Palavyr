94e49c07939796fcb9a6a12a792e07738a654de3
Paul Gradie <paul.e.gradie@gmail.com>
2021-03-31 23:45:27 +1100

    no auth test

--- server/Palavyr.API/Startup.cs
+++ server/Palavyr.API/Startup.cs
@@ -65,8 +65,8 @@ namespace Palavyr.API
             app.UseHttpsRedirection();
             app.UseCors();
             app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
+            // app.UseAuthentication();
+            // app.UseAuthorization();
             app.UseMiddleware<SetHeadersMiddleware>(); // MUST come after UseAuthentication to ensure we are setting these headers on authenticated requests
             app.UseEndpoints(
                 endpoints =>
