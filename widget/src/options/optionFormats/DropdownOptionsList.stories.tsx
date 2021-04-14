ae9184f8edfda5c2634fdbeb52c4d455b2e1571f
Paul Gradie <paul.e.gradie@gmail.com>
2021-04-15 00:00:31 +1000

    remove carosel and add dropdown stories

---
+++ widget/src/options/optionFormats/DropdownOptionsList.stories.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import { DropdownListOptions, DropdownListProps } from "./DropdownOptionsList";
+import { widgetPreferences } from "@test-data/widgetPreferences";
+import { Meta, Story } from "@storybook/react";
+
+export default {
+    title: "WidgetOptions/DropDownListOptions",
+    component: DropdownListOptions,
+    argTypes: {},
+} as Meta;
+
+const Template = (args: DropdownListProps) => <DropdownListOptions {...args} />;
+
+export const Primary: Story<DropdownListProps> = Template.bind({});
+Primary.args = {
+    setSelectedOption: () => null,
+    preferences: widgetPreferences,
+    options: [
+        {
+            areaDisplay: "Area 1",
+            areaId: "abc-1",
+        },
+        {
+            areaDisplay: "Area 2",
+            areaId: "abc-2",
+        },
+        {
+            areaDisplay: "Area 3",
+            areaId: "abc-123",
+        },
+        {
+            areaDisplay: "Area 4",
+            areaId: "abc-123",
+        },
+    ],
+};
