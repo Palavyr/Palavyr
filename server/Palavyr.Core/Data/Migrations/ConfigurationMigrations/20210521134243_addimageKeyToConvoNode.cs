e97d4a4ecdc5dc2f7b9427a9149eb1dba958479d
Paul Gradie <paul.e.gradie@gmail.com>
2021-05-21 23:43:29 +1000

    add migration for imagekey

---
+++ server/Palavyr.Core/Data/Migrations/ConfigurationMigrations/20210521134243_addimageKeyToConvoNode.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Palavyr.Core.Data.Migrations.ConfigurationMigrations
+{
+    public partial class addimageKeyToConvoNode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageS3Key",
+                table: "ConversationNodes",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageS3Key",
+                table: "ConversationNodes");
+        }
+    }
+}
