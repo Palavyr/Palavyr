08dbe2967acdff46b4ef3e9b9834e5bf6a86f732
Paul <paul.e.gradie@gmail.com>
2021-05-26 21:24:50 +1000

    test

--- frontend/src/common/components/customAlert/CustomFade.tsx
+++ frontend/src/common/components/customAlert/CustomFade.tsx
@@ -10,9 +10,6 @@ interface FadeProps {
 
 
 
-
-
-
 const InnerFade = (props: FadeProps, ref: ((instance: HTMLDivElement | null) => void) | React.RefObject<HTMLDivElement> | null | undefined) => {
     const { in: open, children, onEnter, onExited, ...other } = props;
 
