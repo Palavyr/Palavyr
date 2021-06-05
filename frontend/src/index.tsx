fe181c97e2da4e0990a08e55632f53cd99e0fdef
Paul Gradie <paul.e.gradie@gmail.com>
2021-06-05 20:40:44 +1000

    updates

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "aos/dist/aos.css";
-import ReactGA from "react-ga";
-import { googleAnalyticsTrackingId, isDevelopmentStage } from "@api-client/clientUtils";
 
 import { App } from "./App";
-
-if (!isDevelopmentStage()) {
-    ReactGA.initialize(googleAnalyticsTrackingId);
-}
 
 ReactDOM.render(<App />, document.getElementById("root"));
