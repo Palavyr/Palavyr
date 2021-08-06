017e4de77b9946bdb3e5c1d50cddfe2834dacf6a
Paul Gradie <paul.e.gradie@gmail.com>
2021-08-07 01:36:46 +1000

    seed the color generator

--- frontend/src/dashboard/content/activityDashboard/DailyEnquiriesWeekly.tsx
+++ frontend/src/dashboard/content/activityDashboard/DailyEnquiriesWeekly.tsx
@@ -5,11 +5,27 @@ import { useStyles } from "../demo/ColorOptions";
 import { Line } from "react-chartjs-2";
 import { DataPlot } from "./components/DataPlot";
 import { uniqBy } from "lodash";
+import seedrandom from "seedrandom";
 
 type EnqDataSet = {
     label: string;
     data: number[];
+    borderColor: string;
+    fill: boolean;
+    cubicInterpolationMode: string;
+    tension: number;
 };
+
+export const getRandomColor = (seed: number | string) => {
+    var letters = "0123456789ABCDEF".split("");
+    var color = "#";
+    const rng = seedrandom(seed);
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(rng() * 16)];
+    }
+    return color;
+};
+
 const calcualateDailEnquiryByDay = (areaDetails: AreaNameDetails, enquiries: Enquiries) => {
     const dates = enquiries.map((x) => {
         const date = new Date(Date.parse(x.timeStamp));
@@ -19,21 +35,40 @@ const calcualateDailEnquiryByDay = (areaDetails: AreaNameDetails, enquiries: Enq
             dateString: date.toDateString(),
         };
     });
-    const uniqueDates = uniqBy(dates, (x) => x.dateString);
+
+    // const uniqueDates = uniqBy(dates, (x) => x.dateString).sort((a, b) => a.date.getTime() - b.date.getTime());
+
+    const lastSevenDays: Date[] = [];
+    const now = new Date().getDate();
+    for (let index = 0; index < 7; index++) {
+        const newDay = new Date();
+        newDay.setDate(now - index);
+
+        lastSevenDays.push(newDay);
+    }
+    lastSevenDays.reverse();
 
     const enquiryData: EnqDataSet[] = [];
     areaDetails.forEach((detail: AreaNameDetail) => {
         const areaDataResult: number[] = [];
         const areaName = detail.areaName;
         const areaEnquiries = enquiries.filter((enq: EnquiryRow) => enq.areaName === areaName);
-        uniqueDates.forEach((x) => {
-            const enquiriesOnDateInArea = areaEnquiries.filter((enq) => x.dateString === new Date(Date.parse(enq.timeStamp)).toDateString());
+        lastSevenDays.forEach((previousDate) => {
+            const enquiriesOnDateInArea = areaEnquiries.filter((enq) => {
+                const timeStampDate = new Date(Date.parse(enq.timeStamp)).toDateString();
+                const isEqual = previousDate.toDateString() === timeStampDate;
+                return isEqual;
+            });
             areaDataResult.push(enquiriesOnDateInArea.length);
         });
 
         enquiryData.push({
             label: areaName,
             data: areaDataResult,
+            borderColor: getRandomColor(detail.areaName),
+            fill: false,
+            cubicInterpolationMode: "monotone",
+            tension: 0.8,
         });
     });
 
@@ -46,7 +81,7 @@ const calcualateDailEnquiryByDay = (areaDetails: AreaNameDetails, enquiries: Enq
         },
     };
 
-    return { enquiryData, enquiryOptions, uniqueDates };
+    return { enquiryData, enquiryOptions, lastSevenDays };
 };
 
 export type Dataset = {
@@ -84,10 +119,10 @@ export const DailyEnquiriesWeekly = () => {
 
     const loadEnquiries = useCallback(async () => {
         const enquiries = await repository.Enquiries.getEnquiries();
-        const { enquiryData, enquiryOptions, uniqueDates } = calcualateDailEnquiryByDay(areaNameDetails, enquiries);
+        const { enquiryData, enquiryOptions, lastSevenDays } = calcualateDailEnquiryByDay(areaNameDetails, enquiries);
 
         const plotdata: PlotData = {
-            labels: uniqueDates.map((x) => x.dateString),
+            labels: lastSevenDays.map((x) => x.toDateString()),
             datasets: enquiryData,
         };
 
@@ -103,7 +138,7 @@ export const DailyEnquiriesWeekly = () => {
 
     return (
         <DataPlot
-            title="Daily Activity"
+            title="7 Days of Daily Activity"
             subtitle="Learn about the daily activity of your widget, broken down by area"
             hasData={data !== undefined && data && data && data.labels.length > 0}
             loadingSpinner={loadingspinner}
