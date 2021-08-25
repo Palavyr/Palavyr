564b0c562c5fdb14214329879c1be291036a74ca
Paul Gradie <paul.e.gradie@gmail.com>
2021-08-25 17:40:34 +1000

    touch ups footer

--- frontend/src/dashboard/content/designer/fakeWidgetNew/components/components/ConvoHeader/ConvoHeader.tsx
+++ frontend/src/dashboard/content/designer/fakeWidgetNew/components/components/ConvoHeader/ConvoHeader.tsx
@@ -1,7 +1,6 @@
 import { Card, Fade, makeStyles, Tooltip } from "@material-ui/core";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import FaceIcon from "@material-ui/icons/Face";
-import ReplayIcon from "@material-ui/icons/Replay";
 import "./style.scss";
 import { WidgetPreferences } from "@Palavyr-Types";
 import { WidgetContext } from "../../context/WidgetContext";
@@ -28,26 +27,17 @@ const useStyles = makeStyles(theme => ({
     },
     settingsIcon: (props: WidgetPreferences) => ({
         color: theme.palette.getContrastText(props.headerColor),
-        position: "fixed",
-        right: "5px",
-        top: "5px",
+        position: "relative",
+        float: "right",
+        top: "0px",
+        margin: "0.2rem",
         height: "2rem",
         width: "2rem",
         "&:hover": {
             cursor: "pointer",
         },
     }),
-    replayIcon: {
-        color: theme.palette.common.white,
-        position: "absolute",
-        // right: "5px",
-        // bottom: "8px",
-        height: "1.2rem",
-        width: "1.2rem",
-        "&:hover": {
-            cursor: "pointer",
-        },
-    },
+
     headerBehavior: {
         wordWrap: "break-word",
         padding: "1rem",
@@ -90,16 +80,11 @@ export const ConvoHeader = ({ titleAvatar }: ConvoHeaderProps) => {
     const cls = useStyles(preferences);
     return (
         <Card className={cls.header}>
-            {chatStarted && (
-                <Fade in>
-                    <Tooltip open={tipOpen} title="Update your contact details" placement="left">
-                        <FaceIcon ref={ref as any} className={cls.settingsIcon} />
-                    </Tooltip>
-                </Fade>
-            )}
-            <Tooltip title="Restart this chat" placement="left">
-                <ReplayIcon className={cls.replayIcon} onClick={() => window.location.reload()} />
-            </Tooltip>
+            <Fade in>
+                <Tooltip open={tipOpen} title="Update your contact details" placement="left">
+                    <FaceIcon ref={ref as any} className={cls.settingsIcon} />
+                </Tooltip>
+            </Fade>
             {titleAvatar && <img src={titleAvatar} className="avatar" alt="profile" />}
             <div className={cls.headerBehavior} dangerouslySetInnerHTML={{ __html: preferences.chatHeader }} />
         </Card>
--- frontend/src/dashboard/content/designer/fakeWidgetNew/components/components/Footer/BrandingStrip.tsx
+++ frontend/src/dashboard/content/designer/fakeWidgetNew/components/components/Footer/BrandingStrip.tsx
@@ -1,17 +1,14 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles, Tooltip, Typography } from "@material-ui/core";
 import React from "react";
 import { SpaceEvenly } from "./SpaceEvenly";
+import ReplayIcon from "@material-ui/icons/Replay";
 
 const useStyles = makeStyles(theme => ({
     leadingText: {},
     wrapper: {
         fontFamily: "Poppins",
-        // display: "flex",
-        // justifyContent: "center",
-        // bottom: "0px",
-        // alignText: "center",
-        // height: "30px",
-        justifyItems: "center"
+        justifyItems: "center",
+        paddingLeft: "1rem",
     },
     brand: {
         "&:hover": {
@@ -19,23 +16,40 @@ const useStyles = makeStyles(theme => ({
         },
     },
     spacer: {
-        height: "7%",
+        height: "30px",
         width: "100%",
         backgroundColor: "#264B94",
         color: "white",
-        justifyItems: "center"
+        justifyItems: "center",
+    },
+    replayIcon: {
+        color: theme.palette.common.white,
+        fontSize: "1rem",
+        paddingRight: "1rem",
+        "&:hover": {
+            cursor: "pointer",
+        },
+    },
+    iconRoot: {
+        width: "100%",
+        textAlign: "right",
     },
 }));
 export const BrandingStrip = () => {
     const cls = useStyles();
     return (
-        <SpaceEvenly vertical classes={cls.spacer} center>
-            <Typography className={cls.wrapper} variant="caption" align="center" display="inline">
-                Crafted with{" "}
-                <strong className={cls.brand} onClick={() => window.open("https://www.palavyr.com")}>
-                    Palavyr
-                </strong>
-            </Typography>
-        </SpaceEvenly>
+        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }} className={cls.spacer}>
+            <div style={{ height: "100%", alignItems: "center", display: "flex" }}>
+                <Typography className={cls.wrapper} variant="caption">
+                    Crafted with{" "}
+                    <strong className={cls.brand} onClick={() => window.open("https://www.palavyr.com")}>
+                        Palavyr
+                    </strong>
+                </Typography>
+            </div>
+            <div style={{ height: "100%", alignItems: "center", display: "flex" }}>
+                <ReplayIcon classes={{ root: cls.iconRoot }} className={cls.replayIcon} />
+            </div>
+        </div>
     );
 };
