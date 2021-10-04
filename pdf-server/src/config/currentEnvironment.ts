649507cd5b94c000b7959dc32bb8960de52e4512
Paul Gradie <paul.e.gradie@gmail.com>
2021-10-05 01:14:29 +1100

    cleanup

--- pdf-server/src/config/currentEnvironment.ts
+++ pdf-server/src/config/currentEnvironment.ts
@@ -1,13 +1,24 @@
 import { logDebug } from '../logging/logging';
 
+const isWindows = process.platform.startsWith('win');
+if (isWindows) {
+    process.env.NODE_ENV = 'development';
+} else {
+    process.env.NODE_ENV = 'production';
+}
+
 export const currentEnvironment = process.env.NODE_ENV as string;
 
-if (currentEnvironment === undefined) {
+if (isWindows && currentEnvironment === undefined) {
     // logDebug('CURRENTENV not set');
-    // throw new Error('Must Set Node Env during build.');
+    throw new Error('Must Set Node Env during build.');
 }
 
 export const isDevelopmentStage = () => {
-    return true;
-    // return currentEnvironment.toUpperCase() === 'development'.toUpperCase();
+    if (isWindows) {
+        return true;
+        // return currentEnvironment.toUpperCase() === 'development'.toUpperCase();
+    } else {
+        return false;
+    }
 };
