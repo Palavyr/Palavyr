d43c288073334138c8a27a91604ef6c29243e74d
Paul Gradie <paul.e.gradie@gmail.com>
2021-10-08 17:38:33 +1100

    trigger

--- pdf-server/pdfserver_deployment.ps1
+++ pdf-server/pdfserver_deployment.ps1
@@ -32,6 +32,8 @@ Expand-Archive $packageFile -DestinationPath $tempDir -Force
 (Get-Content $unpackedSettings -Raw) -replace '#%{Pdf.Server.Port}%#', '#{Pdf.Server.Port}' | Out-File $unpackedSettings -Force
 ##########################################################################
 
+
+
 # Rezip the lambda package
 
 Compress-Archive "${tempDir}/*" -DestinationPath $packageFile -Force
