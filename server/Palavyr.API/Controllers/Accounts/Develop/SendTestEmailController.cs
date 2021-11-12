50d69ef86671914f8e736b13f96106f7ed36b847
Paul Gradie <paul.e.gradie@gmail.com>
2021-11-12 17:32:11 +1100

    trigger

--- server/Palavyr.API/Controllers/Accounts/Develop/SendTestEmailController.cs
+++ server/Palavyr.API/Controllers/Accounts/Develop/SendTestEmailController.cs
@@ -1,7 +1,27 @@
-﻿namespace Palavyr.API.Controllers.Accounts.Develop
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using Palavyr.Core.Services.EmailService.SmtpEmail;
+
+namespace Palavyr.API.Controllers.Accounts.Develop
 {
-    public class SendTestEmailController
+    public class SendTestEmailController : PalavyrBaseController
     {
-        
+        private readonly ISmtpEmailClient client;
+
+        public SendTestEmailController(ISmtpEmailClient client)
+        {
+            this.client = client;
+        }
+
+        [AllowAnonymous, Microsoft.AspNetCore.Mvc.HttpPost("send-test-email")]
+        public async Task Post()
+        {
+            await client.SendSmtpEmail(
+                "paul.e.gradie@gmail.com",
+                "paul.e.gradie@gmail.com",
+                "TEST EMAIL LOCAL",
+                "<h2>TEST</h2>",
+                "Okay");
+        }
     }
 }
