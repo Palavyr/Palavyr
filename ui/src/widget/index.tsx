8915bc79bd2b60296d9a4dec23bf7a05b1bea3a7
Paul Gradie <paul.e.gradie@gmail.com>
2022-01-23 15:06:06 +1100

    add analytics page visits to the widget

--- ui/src/widget/index.tsx
+++ ui/src/widget/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, useLocation, Switch } from "react-router-dom";
 import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import { PalavyrWidgetTheme } from "./PalavyrWidgetTheme";
 import { WidgetApp } from "./WidgetApp";
@@ -13,21 +13,31 @@ export const WidgetRoutes = () => {
     const location = useLocation();
     WidgetPageView(location.pathname);
     return (
-        <Router>
+        <>
             <Route exact path="/widget" component={WidgetApp} />
             <Route exact path="/test" component={TestComponent} />
-        </Router>
+        </>
+    );
+};
+
+const App = () => {
+    return (
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+            <MuiThemeProvider theme={PalavyrWidgetTheme}>
+                <CssBaseline />
+                <Router>
+                    <Switch>
+                        <WidgetRoutes />
+                    </Switch>
+                </Router>
+            </MuiThemeProvider>
+        </ErrorBoundary>
     );
 };
 
 ReactDOM.render(
     <React.StrictMode>
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <MuiThemeProvider theme={PalavyrWidgetTheme}>
-                <CssBaseline />
-                <WidgetRoutes />
-            </MuiThemeProvider>
-        </ErrorBoundary>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
