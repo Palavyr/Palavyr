65a1085e5ab1b9a35c7424d8869f8a7458c1762b
Paul Gradie <paul.e.gradie@gmail.com>
2022-02-02 23:11:28 +1100

    add migration to add Unit property on pricing strategy meta

---
+++ server/Palavyr.Core/Data/Migrations/ConfigurationMigrations/20220201085758_AddUnitIdsToDynamicTableMeta.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Palavyr.Core.Data.Migrations.ConfigurationMigrations
+{
+    public partial class AddUnitIdsToDynamicTableMeta : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UnitId",
+                table: "DynamicTableMetas",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnitId",
+                table: "DynamicTableMetas");
+        }
+    }
+}
