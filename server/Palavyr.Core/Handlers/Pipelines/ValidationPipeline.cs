4fd40c6f2970d527f6180cb891e8575e21d47ba4
Paul Gradie <paul.e.gradie@gmail.com>
2022-07-24 09:02:47 +1000

    finally upgrade to net6

--- server/Palavyr.Core/Handlers/Pipelines/ValidationPipeline.cs
+++ server/Palavyr.Core/Handlers/Pipelines/ValidationPipeline.cs
@@ -20,7 +20,8 @@ namespace Palavyr.Core.Handlers.Pipelines
 
         public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-            if (!EnumerableExtensions.Any(validators)) return await next();
+            if (!validators.ToList().Any()) return await next();
+            // if (!EnumerableExtensions.Any(validators)) 
             var context = new ValidationContext<TRequest>(request);
             var results = await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
 
