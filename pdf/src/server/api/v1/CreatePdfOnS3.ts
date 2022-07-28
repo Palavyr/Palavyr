57a8956aed7d1c3f5b0bcdca32c360186620961d
Paul Gradie <paul.e.gradie@gmail.com>
2022-07-28 11:55:47 +1000

    package lock and add localstack

--- pdf/src/server/api/v1/CreatePdfOnS3.ts
+++ pdf/src/server/api/v1/CreatePdfOnS3.ts
@@ -5,6 +5,7 @@ import pdf from 'html-pdf';
 import responses from 'http/responses/sendResponse';
 import aws from 'aws-sdk';
 import { ReadStream } from 'fs';
+import { APPLICATION_PDF } from 'http/contentTypes';
 
 export const WriteToS3 = (html: string, options: S3RequestBody, response: Response) => {
   const s3 = new aws.S3(options.s3ClientConfig as aws.S3.ClientConfiguration);
@@ -21,7 +22,7 @@ export const WriteToS3 = (html: string, options: S3RequestBody, response: Respon
       Key: options.key,
       Body: stream,
       Bucket: options.bucket,
-      ContentType: 'application/pdf',
+      ContentType: APPLICATION_PDF,
     };
 
     logTrace('Attempting to upload...not');
@@ -61,7 +62,6 @@ const unpackS3Request = (req: Request): S3RequestBody => {
 export const create_pdf_on_s3_v1 = (app: Application) => {
   app.post('/api/v1/create-pdf-on-s3', async (request: Request, response: Response, next: NextFunction) => {
     logTrace('Received request to create PDF on S3.');
-
     const options = unpackS3Request(request);
     WriteToS3(options.html, options, response);
   });
