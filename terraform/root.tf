50af090141c4cf070693422f9696fcf904b64b0b
Paul Gradie <paul.e.gradie@gmail.com>
2022-08-21 20:07:47 +1000

    migrator

--- terraform/root.tf
+++ terraform/root.tf
@@ -120,3 +120,21 @@ module "server_group" {
 #   key_alias             = "alias/palavyr_key_${lower(var.environment)}-${random_id.rand.hex}"
 #   tags                  = local.tags
 # }
+
+# resource "aws_lambda_function" "migrator" {
+#   function_name = "palavyr-db-migrator-${lower(var.environment)}-${lower(random_id.rand.hex)}"
+#   runtime       = "dotnet6"
+#   handler       = "Palavyr.Data.Migrator::Palavyr.Data.Migrator.MigratorLambdaHandler::MigratorHandler"
+#   role          = aws_iam_role.migrator_role.arn
+
+#   environment {
+#     variables = {
+#       "Palavyr_Environment"      = var.environment
+#       "Palavyr_ConnectionString" = module.database.connection_string
+#     }
+#   }
+#   filename = "./dummy_uploads/dummy_lambda_dotnet.zip"
+
+
+#   tags = local.tags
+# }
