626c9869ba7630e421bf37c7b5d330c1d33f5214
Paul <paul.e.gradie@gmail.com>
2022-10-19 08:43:26 +1100

    update pdf server response prop names

--- server/Palavyr.Core/Services/PdfService/PdfServerResponse.cs
+++ server/Palavyr.Core/Services/PdfService/PdfServerResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace Palavyr.Core.Services.PdfService
+﻿using System.Text.Json.Serialization;
+
+namespace Palavyr.Core.Services.PdfService
 {
     public class PdfServerResponse
     {
+        [JsonPropertyName("s3Key")]
         public string S3Key { get; set; }
+
+        [JsonPropertyName("fileNameWithExtension")]
         public string FileNameWithExtension { get; set; }
+
+        [JsonPropertyName("fileStem")]
         public string FileStem { get; set; }
     }
 }
